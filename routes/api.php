c9604b17c51cb668ac3d8734b95336d73ffde4f6
Phu Quy <dinhphuquy2001@gmail.com>
2024-03-28 23:56:03 +0700

    config cors

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,9 @@ use App\Http\Controllers\ApiWordController;
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
