53da25a24579ac8741b5cf0c8b5d823239ffc8f0
Phu Quy <dinhphuquy2001@gmail.com>
2024-04-16 23:50:01 +0700

    config docker

--- app/Http/Controllers/ApiWordController.php
+++ app/Http/Controllers/ApiWordController.php
@@ -83,8 +83,6 @@ class ApiWordController extends Controller
                 $subPath = "/{$request->examId}{$request->examShiftId}{$request->departmentId}";
                 File::deleteDirectory($this->_PATH_ZIP . $subPath);
                 File::deleteDirectory($this->_PATH_EXTRACTED . $subPath);
-                mkdir($this->_PATH_ZIP . $subPath, 0777, true);
-                mkdir($this->_PATH_EXTRACTED . $subPath, 0777, true);
                 $file = $fileReceived->getFile(); // get file
                 // file danh sách
                 if ($fileType == FileType::LIST) {
